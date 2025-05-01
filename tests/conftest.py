b9d3bc09b702ff7e574aa47bd6a35ec1be10cd2e
MarinaShkunkova <m.shkunkova@yandex.ru>
2025-05-01 19:28:20 +0300

    add setup_browser in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,35 @@
 import pytest
-from selene import browser
+from selene import browser, Browser, Config
 
 from selenium import webdriver
+
+from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    # Задаем базовый урл
-    browser.config.base_url = 'https://demoqa.com'
-    # Задаем размер окна браузера
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    driver_options = webdriver.ChromeOptions()
-    # Selenium WebDriver будет ожидать, пока не будет возвращен запуск события DOMContentLoaded
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    browser = Browser(Config(driver))
+
+    @pytest.fixture(scope='function', autouse=True)
+    def browser_management():
+        browser.config.base_url = 'https://demoqa.com'
 
     yield
 
